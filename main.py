2a42644053d266e94b89f08279ca3f762085c659
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-07-07 12:40:09 +0530

    Groundwork setup

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# import string module
 import string
 
+# set of Uppercase and Lowercase characters
 set1 = string.ascii_letters
 print(set1)
 
+# set of numbers 0-9
 set2 = string.digits
 print(set2)
 
+# set of special characters
 set3 = string.punctuation
 print(set3)
+
+
