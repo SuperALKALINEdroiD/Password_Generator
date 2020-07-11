7e1d0a1c57841089835ddc0379b7e50548f01380
AlkalineD <64575780+SuperALKALINEdroiD@users.noreply.github.com>
2020-07-11 11:28:31 +0530

    HTML linked yay

--- main.py
+++ main.py
@@ -3,44 +3,47 @@ import string
 # import random module
 import random
 # import Flask module
-from flask import Flask
+from flask import Flask, render_template
 
+# set of Uppercase and Lowercase characters
+set1 = string.ascii_letters
 
+# set of numbers 0-9
+set2 = string.digits
+
+# set of special characters
+set3 = string.punctuation
+
+# convert String to list
+
+ls1 = list(set1)
+ls2 = list(set2)
+ls3 = list(set3)
+
+pass_glossary = []
+
+# list1.extend(list2) ===> Similar to appending list but doesnt create list within list
+
+pass_glossary.extend(ls1)
+pass_glossary.extend(ls2)
+pass_glossary.extend(ls3)
+
+# Maximum possible password length ====> 94
 
 app = Flask(__name__)
 @app.route('/')
+def front():
+    return 'Welcome'
+@app.route('/pass')
 def passGen():
-    # set of Uppercase and Lowercase characters
-    set1 = string.ascii_letters
-
-    # set of numbers 0-9
-    set2 = string.digits
-
-    # set of special characters
-    set3 = string.punctuation
-
-    # convert String to list
-
-    ls1 = list(set1)
-    ls2 = list(set2)
-    ls3 = list(set3)
-
-    pass_glossary = []
-
-    # list1.extend(list2) ===> Similar to appending list but doesnt create list within list
-
-    pass_glossary.extend(ls1)
-    pass_glossary.extend(ls2)
-    pass_glossary.extend(ls3)
-
-    #pass_len = int(input("Enter Password Length: "))
-
-    o_list = random.sample(pass_glossary, 5)
+    o_list = random.sample(pass_glossary, 15)
 
     o_str = ''.join(o_list)  # ===> join the elements of password list to give a String
-    return o_str
+    return render_template("formPage.html", password = o_str)
+
+
 
 if __name__ =='__main__':
-    app.run()
+    app.run(debug = True)
 
 
